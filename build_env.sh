ab38b323b800e5b95367d529552cbb338df53612
bene <bene.gaab@web.de>
2019-04-05 14:52:51 +0200

    First version of new build_env script.

--- build_env.sh
+++ build_env.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
 
-
+sudo apt-get install libbluetooth-dev python-virtualenv
+virtualenv -p python2 venv2
+. ./venv2/bin/activate
+wget https://github.com/Eelviny/nxt-python/archive/v2.2.2.zip
+unzip v2.2.2.zip
+cd nxt-python-2.2.2
+python setup.py install
+cd ..
 pip install -r requirements.txt
+python setup.py install
