3020619632d6f0ab901bb862e014b971928be350
bene017 <49234345+bene017@users.noreply.github.com>
2019-04-08 15:04:09 +0200

    Update build_env.sh

--- build_env.sh
+++ build_env.sh
@@ -5,7 +5,7 @@ virtualenv -p python3 venv3 >> LEGO.log
 . ./venv3/bin/activate >> LEGO.log
 wget https://github.com/Eelviny/nxt-python/archive/master.zip >> LEGO.log
 unzip master.zip >> LEGO.log
-cd nxt-python-2.2.2
+cd nxt-python-master
 python setup.py install >> ../LEGO.log
 cd ..
 pip install -r requirements.txt >> LEGO.log
