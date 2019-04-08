4485f173d25f90863f3781a3ec306658a80e1944
bene017 <49234345+bene017@users.noreply.github.com>
2019-04-08 12:26:35 +0200

    Bash Install File V2
    added:
    set -e
    @all << LEGO.log

--- build_env.sh
+++ build_env.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-
-sudo apt-get install libbluetooth-dev python-virtualenv
-virtualenv -p python2 venv2
-. ./venv2/bin/activate
-wget https://github.com/Eelviny/nxt-python/archive/v2.2.2.zip
-unzip v2.2.2.zip
-cd nxt-python-2.2.2
-python setup.py install
-cd ..
-pip install -r requirements.txt
-python setup.py install
+set -e
+sudo apt-get install libbluetooth-dev python-virtualenv << LEGO.log
+virtualenv -p python2 venv2 << LEGO.log
+. ./venv2/bin/activate << LEGO.log
+wget https://github.com/Eelviny/nxt-python/archive/v2.2.2.zip << LEGO.log
+unzip v2.2.2.zip << LEGO.log
+cd nxt-python-2.2.2 << LEGO.log
+python setup.py install << LEGO.log
+cd .. << LEGO.log
+pip install -r requirements.txt << LEGO.log
+python setup.py install << LEGO.log
