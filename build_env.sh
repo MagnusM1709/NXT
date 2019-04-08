b5bafb9e8981b62b686587be01ba1557044df1ee
bene017 <49234345+bene017@users.noreply.github.com>
2019-04-08 15:12:29 +0200

    Update build_env.sh

--- build_env.sh
+++ build_env.sh
@@ -2,7 +2,7 @@
 set -e
 sudo apt-get install libbluetooth-dev python3-virtualenv >> LEGO.log
 virtualenv -p python3 venv3 >> LEGO.log
-. ./venv3/bin/activate >> LEGO.log
+sudo. ./venv3/bin/activate >> LEGO.log
 wget https://github.com/Eelviny/nxt-python/archive/master.zip >> LEGO.log
 unzip master.zip >> LEGO.log
 cd nxt-python-master
